15df5baa01e0b535544efdeb977a9732d3143b05
Aris Karageorgos <aris.karageorgos@gmail.com>
2014-06-18 15:06:43 +0700

    Initial translation setup

--- Module.php
+++ Module.php
@@ -2,14 +2,35 @@
 
 namespace pheme\settings;
 
+use Yii;
+
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'app\modules\settings\controllers';
+
+    public $defaultRoute = 'settings';
 
     public function init()
     {
         parent::init();
 
-        // custom initialization code goes here
+        $this->registerTranslations();
+    }
+
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['extensions/yii2-settings/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@vendor/pheme/yii2-settings/messages',
+            'fileMap' => [
+                'extensions/yii2-settings/settings' => 'settings.php',
+            ],
+        ];
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('extensions/yii2-settings/' . $category, $message, $params, $language);
     }
 }
