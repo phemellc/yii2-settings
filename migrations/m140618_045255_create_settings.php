b302c6ae9889f0df922fce84a1bafc404e4ab92c
Aris Karageorgos <aris.karageorgos@gmail.com>
2014-06-20 15:55:40 +0700

    Adjusted migration

--- migrations/m140618_045255_create_settings.php
+++ migrations/m140618_045255_create_settings.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link http://phe.me
+ * @copyright Copyright (c) 2014 Pheme
+ * @license MIT http://opensource.org/licenses/MIT
+ */
 
 use yii\db\Schema;
 
@@ -6,13 +11,19 @@ class m140618_045255_create_settings extends \yii\db\Migration
 {
     public function up()
     {
-        $this->createTable('settings', [
-            'id' => Schema::TYPE_PK,
-            'type' => Schema::TYPE_STRING,
-            'section' => Schema::TYPE_STRING,
-            'key' => Schema::TYPE_STRING,
-            'value' => Schema::TYPE_TEXT,
-        ]);
+        $this->createTable(
+            'settings',
+            [
+                'id' => Schema::TYPE_PK,
+                'type' => Schema::TYPE_STRING,
+                'section' => Schema::TYPE_STRING,
+                'key' => Schema::TYPE_STRING,
+                'value' => Schema::TYPE_TEXT,
+                'active' => Schema::TYPE_BOOLEAN,
+                'created' => Schema::TYPE_DATETIME,
+                'modified' => Schema::TYPE_DATETIME,
+            ]
+        );
     }
 
     public function down()
