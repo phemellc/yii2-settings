39fa68ad6df1dca9e5fb65546d047ebe287ffe65
Aris Karageorgos <aris.karageorgos@gmail.com>
2014-06-20 15:55:56 +0700

    PHPDoc

--- Module.php
+++ Module.php
@@ -1,21 +1,40 @@
 <?php
 
+/**
+ * @link http://phe.me
+ * @copyright Copyright (c) 2014 Pheme
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
 namespace pheme\settings;
 
 use Yii;
 
+/**
+ * Class Module
+ * @package pheme\settings
+ * @author Aris Karageorgos <aris@phe.me>
+ */
 class Module extends \yii\base\Module
 {
+    /**
+     * @var string The controller namespace to use
+     */
     public $controllerNamespace = 'pheme\settings\controllers';
 
+    /**
+     * Init module
+     */
     public function init()
     {
         parent::init();
-
         $this->registerTranslations();
     }
 
-    public function registerTranslations()
+    /**
+     * Registers the translation files
+     */
+    protected function registerTranslations()
     {
         Yii::$app->i18n->translations['extensions/yii2-settings/*'] = [
             'class' => 'yii\i18n\PhpMessageSource',
@@ -27,6 +46,17 @@ class Module extends \yii\base\Module
         ];
     }
 
+    /**
+     * Translates a message. This is just a wrapper of Yii::t
+     *
+     * @see Yii::t
+     *
+     * @param $category
+     * @param $message
+     * @param array $params
+     * @param null $language
+     * @return string
+     */
     public static function t($category, $message, $params = [], $language = null)
     {
         return Yii::t('extensions/yii2-settings/' . $category, $message, $params, $language);
