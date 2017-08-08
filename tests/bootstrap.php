5b645888dbd37c9703e84fc0e05e7f4d8bd8788e
zhangjunwei <zhangjunwei@lianluo.com>
2017-08-08 18:18:25 +0800

    phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,5 +15,7 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@tests', __DIR__);
+
 Yii::setAlias('@migrate', dirname(__DIR__));
+
 Yii::setAlias('@pheme\settings\messages', dirname(__DIR__) . '/messages');
